bbaa19d263707af5217a79d1018335ce645bf456
prayanshTripathi <prayanshtripathi@gmail.com>
2026-05-08 22:56:10 +0530

    Learn: Arithmetic & Relational Operators | ADD: Arithmetic.java, Relational.java

---
+++ src/BASICs/DATATYPEs/Arithmetic.java
@@ -0,0 +1,70 @@
+package BASICs.DATATYPEs;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        // Arithmetic Operators In JAVA
+        // Five Types Of Arithmetic Operators
+        // +, -, *, /, %
+
+        int a = 15,b = 5;
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+        int g = a % b;
+
+        System.out.println(c + " , " + d + " , " + e + " , " + f + " , " + g);
+
+
+
+        // +=, -=, *=, /=, %=
+        int h = a + 2;
+        System.out.println(h);
+        // h = h + 2; //In Programming this is okay
+        h += 2;
+        System.out.println(h);
+        h -= 2;
+        System.out.println(h);
+        h *= 2;
+        System.out.println(h);
+        h /= 2;
+        System.out.println(h);
+        h %= 2;
+        System.out.println(h);
+
+
+
+        // --, ++ --> Increment & Descrement Operator
+        int i = 6;
+        i++; // i = i + 1     // i += 1
+        System.out.println(i);
+        i--; // i = i - 1     // i -= 1
+        System.out.println(i);
+
+
+
+        // Post Increment/Decrement
+        int j = 10;
+        j++;
+        j++;
+        System.out.println(j++);
+        System.out.println(j);
+
+        // Pre Increment/Decrement
+        int k = 37;
+        System.out.println(--k);
+        System.out.println(++k);
+        System.out.println(--k);
+
+        // j = 13
+        int p = --j;
+        System.out.println(p);
+        int q = j++;
+        System.out.println(q);
+        int r = j;
+        System.out.println(r);
+
+        
+
+    }
+}
---
+++ src/BASICs/DATATYPEs/Relational.java
@@ -0,0 +1,18 @@
+package BASICs.DATATYPEs;
+
+public class Relational {
+    public static void main(String[] args) {
+        // Relational Operator
+        // ==, !=, >=, <=, >, <
+
+
+        int a=5,b=6;
+        System.out.println(a!=b);// Is a is not equal to b ? --> True
+        System.out.println(a==b);// Is a is equal to b ? --> False 
+        System.out.println(a>=b);// Is a is greater than or equal to b ? --> False
+        System.out.println(a<=b);// Is a is smaller than or equal to b ? --> True
+        System.out.println(a>b);// Is a is greater than b ? --> False
+        System.out.println(a<b);// Is a is smaller than b ? --> True
+        
+    }
+}
